dc84f405c8c05484393e43abd4ccd3645e2b52c2
Anton Mushnin <anton.mushnin@gmail.com>
2025-03-07 21:50:19 +0300

    feat: mobile view

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import DegenGambit from './components/DegenGambit';
 import { ThirdwebProvider } from 'thirdweb/react';
 import './styles/fonts.css';
+import { TerminalOutput } from './components/TerminalOutput';
 
 function App() {
-
-
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  const isSmallScreen = window.innerWidth < 1000;
 
   return (
     <div style={{paddingBottom: '30px'}}>
-      <ThirdwebProvider>
-        <DegenGambit />
-      </ThirdwebProvider>
+      {(isMobile || isSmallScreen) ? (
+        <div style={{paddingLeft: '20px'}}>
+          <TerminalOutput text={'This version requires desktop'} setIsSystemTyping={() => {}} />
+        </div>
+      ) : (
+        <ThirdwebProvider>
+          <DegenGambit />
+        </ThirdwebProvider>
+      )}
     </div>
   );
 }
