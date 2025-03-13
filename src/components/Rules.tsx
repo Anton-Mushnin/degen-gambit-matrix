50f6493db435ebf13a5718bc33e0340ad93de96c
Anton Mushnin <anton.mushnin@gmail.com>
2025-03-13 21:10:36 +0300

    feat: contract rules component

---
+++ src/components/Rules.tsx
@@ -0,0 +1,34 @@
+import { useDegenGambitInfo } from '../hooks/useDegenGambitInfo';
+import { contractAddress } from '../config';
+import styles from './Rules.module.css';
+import { useEffect } from 'react';
+import { numbers } from '../config/symbols';
+
+const Rules = () => {
+    const contractInfo = useDegenGambitInfo(contractAddress);
+    useEffect(() => {
+        console.log(contractInfo.data);
+    }, [contractInfo.data]);
+    return ( 
+        <div className={styles.container}>
+            <div className={styles.constants}>
+                <div className={styles.constant}>Contract address: {contractAddress}</div>
+                <div className={styles.constant}>Blocks to act: {contractInfo.data?.blocksToAct}</div>
+                <div className={styles.constant}>Cost to spin: {contractInfo.data?.costToSpin}</div>
+                <div className={styles.constant}>Cost to respin: {contractInfo.data?.costToRespin}</div>
+            </div>
+            <div className={styles.prizes}>
+                <div className={styles.prize}>Rewards:</div>
+                {contractInfo.data?.prizes.map((prize, index) => (
+                    <div key={index} className={styles.prize}>{prize}</div>
+                ))}
+            </div>
+            <div className={styles.symbols}>
+                <div className={styles.symbol}>{`Minor symbols: ${numbers.slice(1, 16).join(', ')}`}</div>
+                <div className={styles.symbol}>{`Major symbols: ${numbers.slice(-3).join(', ')}`}</div>
+            </div>
+        </div>
+    )
+}
+
+export default Rules;
