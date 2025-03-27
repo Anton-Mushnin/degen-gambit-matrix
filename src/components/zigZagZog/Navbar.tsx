e3418bbb2392fbe61c3fc7f4e3d70c0ffdfd4392
Anton Mushnin <anton.mushnin@gmail.com>
2025-03-27 18:14:40 +0300

    time and phase info in  navbar

--- src/components/zigZagZog/Navbar.tsx
+++ src/components/zigZagZog/Navbar.tsx
@@ -2,9 +2,31 @@ import { useActiveAccount } from 'thirdweb/react';
 import styles from "./Navbar.module.css";
 import { getBalance } from '@wagmi/core';
 import { wagmiConfig } from "../../config";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from 'react';
 
-const Navbar = () => {
+const Navbar = ({phase, timeLeft}: {phase: string, timeLeft: number}) => {
+    const [_timeLeft, setTimeLeft] = useState(Math.floor(timeLeft / 1000))
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimeLeft(prevTime => prevTime > 0 ? prevTime - 1 : 0)
+        }, 1000)
+        return () => clearInterval(interval)
+    }, [])
+
+    useEffect(() => {
+        setTimeLeft(Math.floor(timeLeft / 1000))
+    }, [timeLeft])
+
+    const queryClient = useQueryClient()
+
+    useEffect(() => {
+        if (_timeLeft === 0) {
+            queryClient.invalidateQueries({queryKey: ["currentGameAndRoundState"]})
+        }
+    }, [_timeLeft])
+
     const activeAccount = useActiveAccount()
     const balance = useQuery({
         queryKey: ['balance', activeAccount?.address],
@@ -15,11 +37,21 @@ const Navbar = () => {
 
     return (
         <div className={styles.container}>
-            <div className={styles.item}>
-                <span>{`Balance: ${balance.data?.formatted}`}</span>
+            <div className={styles.leftSide}>
+                <div className={styles.item}>
+                    <span>{`Phase: ${phase}`}</span>
+                </div>
+                {_timeLeft > 0 && <div className={styles.item}>
+                    <span>{`Time Left: ${_timeLeft}s`}</span>
+                </div>}
             </div>
-            <div className={styles.getsome} onClick={() => window.open(`https://getsome.game7.io?network=testnet&address=${activeAccount?.address}`, "_blank")}>
-                getsome
+            <div className={styles.rightSide}>
+                <div className={styles.item}>
+                    <span>{`Balance: ${balance.data?.formatted}`}</span>
+                </div>
+                <div className={styles.getsome} onClick={() => window.open(`https://getsome.game7.io?network=testnet&address=${activeAccount?.address}`, "_blank")}>
+                    getsome
+                </div>
             </div>
         </div>
     )
