04cc32ce3c59dcb848318bf1ca5686b809081e44
Anton Mushnin <anton.mushnin@gmail.com>
2025-03-27 18:53:45 +0300

    removing unused modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import { ThirdwebProvider } from 'thirdweb/react';
 import './styles/fonts.css';
-import { TerminalOutput } from './components/TerminalOutput';
-// import Home from './components/Home';
 import ZigZagZog from './components/zigZagZog/ZIgZagZog';
 
 function App() {
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-  const isSmallScreen = window.innerWidth < 1000;
 
   return (
     <div style={{paddingBottom: '0px', width: '100vw', height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-      {(isMobile || isSmallScreen) && false ? (
-        <div style={{paddingLeft: '20px'}}>
-          <TerminalOutput text={'This version requires desktop'} setIsSystemTyping={() => {}} />
-        </div>
-      ) : (
+
         <ThirdwebProvider>
           <ZigZagZog />
-          {/* <Home /> */}
         </ThirdwebProvider>
-      )}
     </div>
   );
 }
