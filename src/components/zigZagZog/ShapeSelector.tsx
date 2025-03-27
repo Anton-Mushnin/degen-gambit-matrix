e20b6fdc4a4b63b8446cfb6468e9646cb2368454
Anton Mushnin <anton.mushnin@gmail.com>
2025-03-27 12:04:29 +0300

    shape selector

--- src/components/zigZagZog/ShapeSelector.tsx
+++ src/components/zigZagZog/ShapeSelector.tsx
@@ -1,32 +1,63 @@
 import { ShapeSelection } from "@/utils/signing";
 import styles from "./ShapeSelector.module.css";
+import Circle from "./shapes/Circle";
+import Triangle from "./shapes/Triangle";
+import Square from "./shapes/Square";
+import { useEffect } from "react";
 
 
 const ShapeSelector = ({playsCount, selected, onSelect, isCommitPhase}: {playsCount: number, selected: ShapeSelection, onSelect: (shapes: ShapeSelection) => void, isCommitPhase: boolean}) => {
-    const handleShapeClick = (shape: keyof ShapeSelection) => {
-        if (playsCount > selected.circles + selected.triangles + selected.squares && isCommitPhase) {
-            onSelect({...selected, [shape]: selected[shape] + BigInt(1)});
+    const getStrokeColor = (count: bigint) => {
+        if (count === BigInt(0)) return 'gray';
+        const saturation = Math.max(10, (Number(count) - 1) / (playsCount - 1) * 100);
+        return `hsl(270, ${saturation}%, 50%)`;
+    };
+
+    useEffect(() => {
+        console.log(playsCount)
+    }, [playsCount])
+
+    const handleShapeClick = (shape: keyof ShapeSelection, d: number) => {
+        if (!isCommitPhase) {
+            return
+        }
+        const newCount = selected[shape] + BigInt(d);
+
+        if ((playsCount > selected.circles + selected.triangles + selected.squares || d < 0) && isCommitPhase && newCount >= BigInt(0)) {
+            onSelect({...selected, [shape]: newCount});
         }
     }
     return (
         <div className={styles.container}>
             <div className={styles.shapeContainer}>
-                <div className={styles.shape} onClick={() => handleShapeClick("circles")}>
-                    <div className={styles.shapeCircle}>CCC</div>
+                <div className={styles.shape}>
+                    <Circle stroke={getStrokeColor(selected.circles)} />
+                    <div className={styles.shapeCount}>{selected.circles}</div>
                 </div>
-                <div className={styles.shapeCount}>{selected.circles}</div>
+                <div className={styles.buttons}>
+                    <div className={styles.plusButton} onClick={() => handleShapeClick("circles", -1)}><div className={styles.caption}>less</div></div>
+                    <div className={styles.plusButton} onClick={() => handleShapeClick("circles", 1)}><div className={styles.caption}>more</div></div>
+                </div>
             </div>
             <div className={styles.shapeContainer}>
-                <div className={styles.shape} onClick={() => handleShapeClick("triangles")}>
-                    <div className={styles.shapeTriangle}>TTT</div>
+                <div className={styles.shape}>
+                    <Triangle fill={getStrokeColor(selected.triangles)} />
+                    <div className={styles.shapeCount}>{selected.triangles}</div>
                 </div>
-                <div className={styles.shapeCount}>{selected.triangles}</div>
+                <div className={styles.buttons}>
+                    <div className={styles.plusButton} onClick={() => handleShapeClick("triangles", -1)}><div className={styles.caption}>less</div></div>
+                    <div className={styles.plusButton} onClick={() => handleShapeClick("triangles", 1)}><div className={styles.caption}>more</div></div>
+                </div>
             </div>
             <div className={styles.shapeContainer}>
-                <div className={styles.shape} onClick={() => handleShapeClick("squares")}>
-                    <div className={styles.shapeSquare}>SSS</div>
+                <div className={styles.shape}>
+                    <Square stroke={getStrokeColor(selected.squares)} />
+                    <div className={styles.shapeCount}>{selected.squares}</div>
                 </div>
-                <div className={styles.shapeCount}>{selected.squares}</div>
+                <div className={styles.buttons}>
+                    <div className={styles.plusButton} onClick={() => handleShapeClick("squares", -1)}><div className={styles.caption}>less</div></div>
+                    <div className={styles.plusButton} onClick={() => handleShapeClick("squares", 1)}><div className={styles.caption}>more</div></div>
+                </div>
             </div>
         </div>
     )
