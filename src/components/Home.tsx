bdc1262b18dcd33490ef96d88607262fd55d2943
Anton Mushnin <anton.mushnin@gmail.com>
2025-05-26 22:10:20 +0300

    refactor: update Home component to integrate RockPaperScissors and comment out DegenGambit and Rules components

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,20 @@
 import ContractInfo from "./degen-gambit/ContractInfo";
 import DegenGambit from "./degen-gambit/DegenGambit";
+import RockPaperScissors from "./rock-paper-scissors/RockPaperScissors";
 import styles from "./Home.module.css";
 import Rules from "./degen-gambit/Rules";
 import Stream from "./degen-gambit/Stream";
+
 const Home = () => {
     return (
         <div className={styles.container}>
-            <div className={styles.stack} style={{borderRight: '1px solid #636363'}}>
+            {/* <div className={styles.stack} style={{borderRight: '1px solid #636363'}}>
                 <ContractInfo />
                 <Stream />
-            </div >
+            </div > */}
             <div className={styles.stack} style={{flex: '1'}}>
-                <DegenGambit />
-                <Rules />
+                <RockPaperScissors />
+                {/* <Rules /> */}
             </div>
         </div>
     );
