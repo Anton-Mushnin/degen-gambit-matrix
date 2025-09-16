ecf7f6ba2245cd4710be82d7327ffdcd902fbb22
Anton Mushnin <anton.mushnin@gmail.com>
2025-09-16 15:04:39 +0300

    refactor: Remove unused styles import in DegenGambit Stream component
    - Eliminated the unused styles import from Stream.tsx to clean up the code and improve maintainability.

--- src/games/degen-gambit/components/Stream.tsx
+++ src/games/degen-gambit/components/Stream.tsx
@@ -2,7 +2,6 @@ import GameStream from '../../../components/GameStream';
 import { degenGambitABI } from '../../../ABIs/DegenGambit.abi.ts';
 import { degenGambitStreamConfig } from '../config/streamConfig';
 import { degenGambitGame } from '../index';
-import styles from './Stream.module.css';
 
 const Stream: React.FC = () => {
   const contractAddress = degenGambitGame.config.contractAddress as string;
@@ -12,7 +11,6 @@ const Stream: React.FC = () => {
       contractAddress={contractAddress}
       abi={degenGambitABI}
       eventConfigs={degenGambitStreamConfig}
-      className={styles.container}
     />
   );
 };
