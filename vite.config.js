def78b0116d25f5e12c46ae644d00cc5dfbe9f18
Anton Mushnin <anton.mushnin@gmail.com>
2025-10-07 12:14:34 +0300

    Add @types/node package and update Vite config for path resolution
    - Added the @types/node package to package.json and package-lock.json for improved TypeScript support.
    - Updated the Vite configuration to use the 'resolve' method for path aliasing, enhancing module resolution and maintaining consistency in the project structure.
    - This change aims to improve development experience and maintainability of the codebase.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+var __filename = fileURLToPath(import.meta.url);
+var __dirname = dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
@@ -10,7 +12,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src')
-        }
-    }
+            '@': resolve(__dirname, './src'),
+        },
+    },
 });
